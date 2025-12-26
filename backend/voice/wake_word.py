9806566260ed161416935abf85dd4ff01f852537
Harshil <harshilwaghela189@gmail.com>
2025-12-26 11:41:27 +0530

    feat: wake-word detection using Porcupine

---
+++ backend/voice/wake_word.py
@@ -0,0 +1,22 @@
+import pvporcupine
+import sounddevice as sd
+import struct
+
+porcupine = pvporcupine.create(
+    access_key="YOUR_PICOVOICE_KEY",
+    keywords=["hey aiva"]
+)
+
+def listen(callback):
+    def audio_cb(indata, frames, time, status):
+        pcm = struct.unpack_from("h" * frames, indata)
+        if porcupine.process(pcm) >= 0:
+            callback()
+
+    with sd.InputStream(
+        samplerate=porcupine.sample_rate,
+        channels=1,
+        dtype="int16",
+        callback=audio_cb
+    ):
+        sd.sleep(10**9)
